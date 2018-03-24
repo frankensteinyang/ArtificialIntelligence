8bc5672c07dd8a26580791020285b6a94ea1aab7
杨进忠 <fred.is.almighty@usa.com>
2018-03-24 23:57:24 +0800

    Boring
    …

---
+++ CLearning.c
@@ -0,0 +1,8 @@
+void test() {
+	enum Season {Spring, Summer, Autumn, Winter};
+	enum Season s = Winter;
+}
+
+void test1() {
+	enum Season {Spring, Summer, Autumn, Winter};
+}
--- PythonLearning.py
+++ PythonLearning.py
@@ -1,4 +1,8 @@
+import os
 print('Hello, World');
 print(type(2 + 1.3))
 print(type(2/3))
 print(type(2//2))
+print(type([2,3,4,5,6]))
+
+list1 = ['万人空巷', '饕餮盛宴', '醍醐灌顶', '无精打采']
